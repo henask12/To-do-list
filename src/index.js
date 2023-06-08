c518156392f4153fd3f6b915bb3ac9cbbbaca2dd
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-06-08 10:32:34 +0300

    eslint fix

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
-import "./style.css";
+import './style.css';
 
-let tasks = [
-    { description: "Task 1", completed: false, index: 1 },
-    { description: "Task 2", completed: true, index: 2 },
-    { description: "Task 3", completed: false, index: 3 }
+const tasks = [
+  { description: 'Task 1', completed: false, index: 1 },
+  { description: 'Task 2', completed: true, index: 2 },
+  { description: 'Task 3', completed: false, index: 3 },
 ];
 
-const taskList = document.getElementById("todo-list");
+const taskList = document.getElementById('todo-list');
 
 const populateTaskList = () => {
-    taskList.innerHTML = "";
+  taskList.innerHTML = '';
 
-    tasks.forEach((task) => {
-        const listItem = document.createElement("li");
+  tasks.forEach((task) => {
+    const listItem = document.createElement('li');
 
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.classList.add("checkbox");
-        checkbox.checked = task.completed;
-        checkbox.addEventListener("change", () => {
-            task.completed = checkbox.checked;
-            listItem.classList.toggle("completed", task.completed);
-        });
-
-        const taskDescription = document.createElement("span");
-        taskDescription.textContent = task.description;
-
-        listItem.appendChild(checkbox);
-        listItem.appendChild(taskDescription);
-
-        if (task.completed) {
-            listItem.classList.add("completed");
-        }
-
-        taskList.appendChild(listItem);
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('checkbox');
+    checkbox.checked = task.completed;
+    checkbox.addEventListener('change', () => {
+      task.completed = checkbox.checked;
+      listItem.classList.toggle('completed', task.completed);
     });
+
+    const taskDescription = document.createElement('span');
+    taskDescription.textContent = task.description;
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(taskDescription);
+
+    if (task.completed) {
+      listItem.classList.add('completed');
+    }
+
+    taskList.appendChild(listItem);
+  });
 };
 
 populateTaskList();
